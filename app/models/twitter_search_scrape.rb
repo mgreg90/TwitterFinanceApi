0b75569bdbeb6b80da2f424a7078009da4480261
Michael Gregory <mgregory8219@gmail.com>
2016-08-24 16:45:55 -0400

    twitter search works successfully with dummy data. Need to add a way to send info to this method.

--- app/models/twitter_search_scrape.rb
+++ app/models/twitter_search_scrape.rb
@@ -5,11 +5,18 @@ class TwitterSearchScrape < ApplicationRecord
   require 'capybara/poltergeist'
 
   include Capybara::DSL
-  Capybara.default_driver = :poltergeist # for production
-  # Capybara.default_driver = :selenium # for testing
+  # Capybara.default_driver = :poltergeist # for production
+  Capybara.default_driver = :selenium # for testing
 
   def twitter_advanced_search
     visit "https://twitter.com/search-advanced"
+    fill_in 'All of these words', with: 'Kim Kardashian'
+    select 'English (English)', from: 'Written in'
+    within :css, 'div#datepicker' do
+      fill_in 'From this date', with: "2015-08-21"
+      fill_in 'to', with: "2015-09-21"
+    end
+    click_button 'Search'
   end
 
 end
