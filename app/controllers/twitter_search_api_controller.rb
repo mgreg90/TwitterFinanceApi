ecbdfe248f5eb2ad94801de62d0225c35018336e
Michael Gregory <mgregory8219@gmail.com>
2016-08-24 15:05:19 -0400

    twitter search thru api works. Unfortunately, their API only allows me to search tweets going back seven days. That is insufficient for my goal of comparing the data to stock price movements. Working towards a solution.

--- app/controllers/twitter_search_api_controller.rb
+++ app/controllers/twitter_search_api_controller.rb
@@ -1,2 +1,21 @@
 class TwitterSearchApiController < ApplicationController
+
+  def search
+    new_search = TwitterSearchApi.new
+    result = new_search.search_by_term_and_date(cleaned_search_params['term'],
+                                                cleaned_search_params['date'])
+    render json: result
+  end
+
+  private
+  def search_params
+    params.permit('date', 'term')
+  end
+
+  def cleaned_search_params
+    new_params = search_params
+    new_params['date'] = new_params['date'].gsub('_', ' ')
+    new_params
+  end
+
 end
--- app/models/twitter_search_api.rb
+++ app/models/twitter_search_api.rb
@@ -1,8 +1,8 @@
 class TwitterSearchApi < ApplicationRecord
 
   def search_by_term_and_date(term, date) # date should be YYYY-MM-DD
-    @search_results = $twitter.search(term, {date: date, result_type: "mixed",
-      count: 10})
+    @search_results = $twitter.search(term, {until: date, result_type: "popular",
+      lang: 'en', count: 100})
   end
 
 end
